5d1624e239a4f91020ce1259ae70102bb2bd8ea4
David Kinkaku <davidkinkaku1@gmail.com>
2021-09-29 11:24:25 -0500

    backend done

--- server/routes/chat.router.js
+++ server/routes/chat.router.js
@@ -5,13 +5,15 @@ const router = express.Router();
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
+router.get('/:id', (req, res) => {
   // GET route code here
   // this gets all my links from the 
   // SELECT * FROM "conversation" where "user_id" ="req.user.id" order by "id";
-console.log('this is my user', req.user)
-  let queryText = `SELECT * FROM "conversation" WHERE "user_id"=$1 ORDER BY "id";`; 
-    pool.query(queryText,[req.user.id])
+console.log('this is my chat id', req.params.id)
+  let queryText = `SELECT "message"."message", "message"."is_answered", "message"."sent_at" FROM "message"
+  WHERE "conversation_id" =$1
+  ORDER BY "id";`; 
+    pool.query(queryText,[req.params.id])
     .then((result) => {
         res.send(result.rows);
     }).catch((err) => {
@@ -20,25 +22,22 @@ console.log('this is my user', req.user)
 
 });
 
-
 /**
  * POST route template
  */
-
 router.post('/', (req, res) => {
   // POST route code here
-  const newLink = req.body;
-  console.log('this is the body' ,newLink);
+  const newMessage = req.body;
+  console.log('this is the new Message' ,newMessage);
 
+  const message = newMessage.message;
+  const conversationId = newMessage.conversation_id;
+console.log('this is the message added', message, conversationId);
 
-  const url = newLink.url;
-  const userId = req.user.id;
-console.log('this is the url', url);
-
-  const queryText = `INSERT INTO "conversation" ("url", "user_id")
+  const queryText = `INSERT INTO "message" ( "message", "conversation_id")
   VALUES($1, $2);`;
 
-  pool.query(queryText, [url, userId])
+  pool.query(queryText, [message, conversationId])
   .then((result) => {
       res.sendStatus(201);
   }).catch((err) => {
@@ -47,5 +46,54 @@ console.log('this is the url', url);
   })
 
 });
+
+
+// Delete messages by id 
+router.delete('/:id',  (req, res) => { //calling the database
+    let messageId = req.params.id;
+    console.log(messageId);
+    let queryText = `DELETE FROM "message" WHERE "id" = $1;`; 
+    pool.query(queryText, [messageId])
+    .then((result) => {
+        res.sendStatus(204);
+    }).catch((err) => {
+        res.sendStatus(500);
+    })
+});
+
+//delete by id 
+// localhost:5000/songs/delete/2
+router.delete('/:id',  (req, res) => { //calling the database
+    let conversationId = req.params.id;
+    
+    console.log('this is the convo id', conversationId);
+    let queryText = `DELETE FROM "conversation" WHERE "id" = $1 AND "user_id"=$2;`; 
+    pool.query(queryText, [ conversationId, req.user.id])
+    .then((result) => {
+        res.sendStatus(204);
+    }).catch((err) => {
+        res.sendStatus(500);
+    })
+});
+
+// update messages to answer of a song
+
+router.put('/:id', (req, res) => {
+    let messageId = req.params.id;
+    console.log('This inside the put: ', messageId);
+
+    let queryText = `UPDATE "message" SET "is_answered" = NOT is_answered  
+    WHERE "id" = $1;`
+
+    pool.query(queryText, [ messageId])
+    .then((result) => {
+        res.sendStatus(200)
+    }).catch((err) => {
+        console.log('server error', err)
+    })
+})
+
+
+
 
 module.exports = router;
